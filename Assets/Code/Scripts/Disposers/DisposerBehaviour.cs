fc3ae9c6a658c9aebf81dab19fc78f251f19b422
Supykes <supykess@gmail.com>
2023-11-28 14:41:24 +0200

    added disposer models; added disposers behaviour

---
+++ Assets/Code/Scripts/Disposers/DisposerBehaviour.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+
+public enum DisposerType
+{
+    QualityDisposer,
+    DamagedDisposer
+}
+
+public class DisposerBehaviour : MonoBehaviour
+{
+    [SerializeField] DisposerType disposerType;
+
+    void OnTriggerEnter(Collider obj)
+    {
+        Product product = obj.GetComponent<Product>();
+
+        if (disposerType.Equals(DisposerType.QualityDisposer))
+        {
+            if (!product.isDamaged)
+            {
+                if (product.isCodeEntered)
+                {
+                    MoneyManager.totalMoney++;
+                }
+                else
+                {
+                    MoneyManager.totalMoney--;
+                }
+            }
+            else
+            {
+                MoneyManager.totalMoney--;
+            }
+        }
+        else if (disposerType.Equals(DisposerType.DamagedDisposer))
+        {
+            if (product.isDamaged)
+            {
+                MoneyManager.totalMoney++;
+            }
+            else
+            {
+                MoneyManager.totalMoney--;
+            }
+        }
+
+        ProductsManager.Products.Remove(product);
+
+        StartCoroutine(WaitToDestroyObject(obj));
+    }
+
+    IEnumerator WaitToDestroyObject(Collider obj)
+    {
+        yield return new WaitForSeconds(1f);
+
+        Destroy(obj.gameObject);
+    }
+}
